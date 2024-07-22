78e7c4fd0fc97b8d399a0f9b13094f69102aac12
ivan.scherer <ivan.scherer@samsistemas.com.ar>
2024-07-22 20:32:53 -0300

    subo parte login google amend

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
-import '../styles/global.css'
-
 import { Inter } from 'next/font/google'
-
+import Providers from '../app/providers'
+import { SessionProvider } from "next-auth/react"
 const inter = Inter({ subsets: ['latin'] })
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     return (
         <main className={inter.className}>
-            <Component {...pageProps} />
+            <SessionProvider session={session}>
+                <Providers>
+                    <Component {...pageProps} />
+                </Providers>
+            </SessionProvider>
         </main>
     )
-}
+} 

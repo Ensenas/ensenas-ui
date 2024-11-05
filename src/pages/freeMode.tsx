6af1899fbaba06c6b4f858338f8f6e5586f1e5be
MateoCoiro <mcoiro@frba.utn.edu.ar>
2024-11-05 20:07:58 -0300

    add freeMode

--- src/pages/freeMode.tsx
+++ src/pages/freeMode.tsx
@@ -3,14 +3,30 @@
 /* eslint-disable no-console */
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
 import { io, Socket } from 'socket.io-client'
+import styled from 'styled-components'
 
-import HomeLayout from '../components/HomeLayout/HomeLayout'
-import ProtectedRoute from '../components/ProtectedRoute'
 import Spinner from '../components/Spinner/Spinner'
-import { Container, Overlay, SelectorsContainer, StyledSelect, WebcamContainer } from '../styles/FreeMode.styles'
+import { Container, Overlay, WebcamContainer } from '../components/Recorder/RecorderElements'
+
+const PreviewContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 200px;
+  height: 150px;
+  border: 2px solid #000;
+  overflow: hidden;
+  z-index: 10;
+`
+
+const MainImageContainer = styled.div`
+  position: relative;
+  width: 70%;
+  height: 620px;
+`
 
 const unitWords = {
     familiares: ['papa', 'mama', 'hijo', 'hermana'],
@@ -21,21 +37,20 @@ const unitWords = {
     frases_ii: ['color', 'estudiar', 'favorito', 'hermana', 'mi', 'rojo', 'universidad']
 }
 
-const FreeMode: React.FC = () => {
+export default function VideoStreamRemoto() {
     const [socket, setSocket] = useState<Socket | null>(null)
-    const [mode, setMode] = useState('Corregir')
-    const [unit, setUnit] = useState('familiares')
     const [border, setBorder] = useState('unset')
     const [isConnected, setIsConnected] = useState(false)
-    const [reset, setReset] = useState(false)
-    const [selectedWord, setSelectedWord] = useState(unitWords.familiares[0])
-    const [fps, setFps] = useState(0)
+    const [selectedUnit, setSelectedUnit] = useState('familiares')
     const webcamRef = useRef<Webcam>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const outputRef = useRef<HTMLImageElement>(null)
 
     useEffect(() => {
         const newSocket = io('wss://alarma.mywire.org:3050')
+        setSocket(newSocket)
+
+        let processingFrame = false
 
         newSocket.on('connect', () => {
             console.log('Socket connected:', newSocket.id)
@@ -45,35 +60,31 @@ const FreeMode: React.FC = () => {
 
         newSocket.on('disconnect', (reason) => {
             console.log('Socket disconnected:', reason)
+            setIsConnected(false)
         })
+
         newSocket.on('processed_frame', (data) => {
-            if (outputRef.current) {
+            if (processingFrame) return // Ignorar si ya se está procesando otro frame
+            processingFrame = true
+
+            if (outputRef.current && data.image) {
                 outputRef.current.src = data.image
             }
+
+            processingFrame = false// Ajusta el delay según tu preferencia
         })
-        setSocket(newSocket)
-        newSocket.emit('unit_selected', { unidad: unit })
 
-        const handleKeyPress = (event) => {
-
-            if (event.keyCode === 13) {
-                setReset(true)
-                setBorder('3px solid #039619')
-            }
-        }
-
-        document.addEventListener('keydown', handleKeyPress)
-
+        newSocket.emit('unit_selected', { unidad: selectedUnit })
+        console.log("Sent unit_selected event to server with unit:", selectedUnit)
 
         return () => {
             newSocket.disconnect()
-            document.removeEventListener('keydown', handleKeyPress)
             console.log('Socket disconnected:', newSocket.id)
         }
-    }, [])
+    }, [selectedUnit])
 
-    const sendFrame = () => {
-        if (webcamRef.current && canvasRef.current) {
+    const sendFrame = useCallback(() => {
+        if (webcamRef.current && canvasRef.current && socket) {
             const video = webcamRef.current.video
             const canvas = canvasRef.current
             const context = canvas.getContext('2d')
@@ -84,79 +95,75 @@ const FreeMode: React.FC = () => {
                 context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
 
                 const dataURL = canvas.toDataURL('image/jpeg', 0.5)
-                if (socket) {
-                    socket.emit('video_frame', { image: dataURL, reset: reset })
-                    setReset(false)
-                }
+                socket.emit('video_frame', { image: dataURL })
             }
         }
-    }
+    }, [socket])
 
     useEffect(() => {
         const interval = setInterval(sendFrame, 250)
         return () => clearInterval(interval)
-    }, [socket, sendFrame])
+    }, [sendFrame])
 
     const handleUnitChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const newUnit = e.target.value as keyof typeof unitWords
-        setUnit(newUnit)
-        setSelectedWord(unitWords[newUnit][0])
+        setSelectedUnit(newUnit)
         socket?.emit('unit_selected', { unidad: newUnit })
     }
 
-    const handleModeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const newMode = e.target.value
-        setMode(newMode)
-        socket?.emit('reset_text')
-    }
-
-    const handleWordChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedWord(e.target.value)
-        socket?.emit('reset_text')
-    }
-
     return (
-        <ProtectedRoute>
-            <HomeLayout activePage='/freeMode'>
-                {isConnected ? (
-                    <div>
-                        <Container>
-                            <div id="fpsDisplay">FPS Max: {fps}</div>
-
-                            <SelectorsContainer>
-                                <StyledSelect value={unit} onChange={handleUnitChange}>
-                                    <option value="familiares">Familiares</option>
-                                    <option value="colores">Colores</option>
-                                </StyledSelect>
-
-                            </SelectorsContainer>
-
-                            <img ref={outputRef} style={{ width: '70%', height: '620px', border: border }} />
-
-                            <WebcamContainer>
+        <div>
+            {isConnected ? (
+                <div>
+                    <Container>
+                        <MainImageContainer>
+                            <img
+                                ref={outputRef}
+                                style={{ width: '100%', height: '100%', border: border, objectFit: 'cover' }}
+                                alt="Processed output"
+                            />
+                            <PreviewContainer>
                                 <Webcam
                                     ref={webcamRef}
                                     audio={false}
                                     videoConstraints={{
                                         facingMode: 'user',
-                                        width: 1920,
-                                        height: 1080
+                                        width: 1280,
+                                        height: 720
                                     }}
-                                    style={{ opacity: 0 }}
+                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                                 />
-                            </WebcamContainer>
+                            </PreviewContainer>
+                        </MainImageContainer>
 
-                            <canvas ref={canvasRef} style={{ display: 'none' }} />
+                        <WebcamContainer>
+                            <Webcam
+                                audio={false}
+                                videoConstraints={{
+                                    facingMode: 'user',
+                                    width: 1920,
+                                    height: 1080
+                                }}
+                                style={{ opacity: '0' }}
+                            />
+                        </WebcamContainer>
 
-                            <Overlay />
-                        </Container>
-                    </div>
-                ) : (
-                    <Spinner />
-                )}
-            </HomeLayout>
-        </ProtectedRoute>
+                        <select value={selectedUnit} onChange={handleUnitChange}>
+                            <option value="familiares">Familiares</option>
+                            <option value="colores">Colores</option>
+                            <option value="pronombres">Pronombres</option>
+                            <option value="saludos">Saludos</option>
+                            <option value="frases_i">Frases I</option>
+                            <option value="frases_ii">Frases II</option>
+                        </select>
+
+                        <canvas ref={canvasRef} style={{ display: 'none' }} />
+                        <Overlay />
+                    </Container>
+                </div>
+            ) : (
+                <Spinner />
+            )}
+        </div>
     )
 }
-
-export default FreeMode
